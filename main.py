98b4cf98a6fa17a1ba08c6dbf4938c69b16ad807
brunodev12 <bruno.carthery12@gmail.com>
2024-02-15 23:44:32 -0300

    feat: new version

--- main.py
+++ main.py
@@ -1,26 +1,83 @@
-from utils.get_access_token import getAccessToken
+import time
+import threading
+from services.list_token import listToken
+from services.send_offer import sendOffer
 from services.take_bid_services import takeBidServices
+from utils.get_access_token import getAccessToken
+from utils.get_collections import getCollectionsData
 from utils.get_user_assets import getUserAssets
+from utils.beth_balance import getBETHbalance
+from utils.helpers import newDataTakeBid
+from utils.order_data import orderDataTakeBid, orderUserData
+
+
+def startListing(user_tokens:dict):
+    tokens:list[dict] = user_tokens.get('tokens', [])
+    for item in tokens:
+        found:bool = item['found']
+        listing_enabled:bool = item.get('listing enabled', False)
+        if found and listing_enabled:
+            listToken(item)
+
+def startTakeOffers(new_user_tokens:list):
+    for item in new_user_tokens:
+        take_bids_enabled:bool = item['take bids enabled']
+        if take_bids_enabled:
+            takeBidServices(item)
+
+def startSendOffers(item:dict):
+    bought:bool = item['bought']
+    offers_enabled:bool = item['offers enabled'] == "yes"
+    if not bought and offers_enabled:
+        sendOffer(item)
+
 
 def run():
 
-    getAccessToken()
+    thread1 = threading.Thread(target=getAccessToken)
+    thread2 = threading.Thread(target=getBETHbalance)
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
 
     user_tokens = getUserAssets()
 
-    new_user_tokens = []
+    collection_info = getCollectionsData()
 
-    for item in user_tokens:
-        new_user_tokens.append(item)
-        if len(item['tokens']) > 1:
-            for token in item['tokens']:
-                new_item = {'contract': item['contract'], 'tokens': [token]}
-                new_user_tokens.append(new_item)
+    orderUserData(user_tokens, collection_info)
+    orderDataTakeBid(user_tokens)
 
-    for i in new_user_tokens:
-        tokens = i['tokens']
-        contract = i['contract']
-        takeBidServices(tokens, contract)
+    new_user_tokens = newDataTakeBid(user_tokens)
+
+    thread1 = threading.Thread(target=startListing, args=(user_tokens,))
+    thread2 = threading.Thread(target=startTakeOffers, args=(new_user_tokens,))
+
+    thread1.start()
+    time.sleep(0.5)
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+    chunk_size = 10
+
+    chunks = [collection_info[i:i + chunk_size] for i in range(0, len(collection_info), chunk_size)]
+
+    for chunk in chunks:
+
+        threads = []
+        for item in chunk:
+            thread = threading.Thread(target=startSendOffers, args=(item,))
+            threads.append(thread)
+            time.sleep(0.2)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
 
 if __name__ == "__main__":
     run()
