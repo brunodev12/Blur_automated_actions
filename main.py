cf6b7058d12435147fc2753433b6023bd912393e
brunodev12 <bruno.carthery12@gmail.com>
2024-07-27 22:02:44 -0300

    feat: added blast chain

--- main.py
+++ main.py
@@ -4,42 +4,52 @@ from services.list_token import listToken
 from services.send_offer import sendOffer
 from services.take_bid_services import takeBidServices
 from utils.db_data_utils import getAllDataDB, lastSalePriceAll_utils, saveAllDataDB
-from utils.get_access_token import getAccessToken
+from utils.get_access_token import getAllAccessTokens
 from utils.get_collections import getCollectionsData
-from utils.get_user_assets import getUserAssets
+from utils.get_user_assets import getUserAssetsUtils
 from utils.beth_balance import getBETHbalance
 from utils.helpers import newDataTakeBid
 from utils.order_data import orderDataTakeBid, orderUserData
+from data.constants import blockchains
 
 
 def startListing(user_tokens:dict):
     tokens:list[dict] = user_tokens.get('tokens', [])
     for item in tokens:
         found:bool = item['found']
+        chain:str = item.get('chain','ethereum')
         listing_enabled:bool = item.get('listing enabled', False)
-        if found and listing_enabled:
+        if found and listing_enabled and (chain in blockchains):
             listToken(item)
 
-def startTakingOffers(new_user_tokens:list):
+def startTakingOffers(new_user_tokens:list[dict]):
     for item in new_user_tokens:
+        chain:str = item['chain']
         take_bids_enabled:bool = item['take bids enabled']
-        if take_bids_enabled:
+        if take_bids_enabled and (chain in blockchains):
             takeBidServices(item)
 
 def startSendingOffers(item:dict):
     bought:bool = item['bought']
+    chain:str = item['chain']
     itemsOwned = int(item.get('itemsOwnedByContract',0))
     min_num_assets_per_contract = int(item['min num assets per contract'])
     max_num_assets_per_contract = int(item['max num assets per contract'])
     offers_enabled:bool = item['offers enabled'] == "yes"
-    if not bought and offers_enabled and itemsOwned>=min_num_assets_per_contract and itemsOwned<max_num_assets_per_contract:
+    condition_1:bool = not bought
+    condition_2:bool = offers_enabled
+    condition_3:bool = itemsOwned>=min_num_assets_per_contract
+    condition_4:bool = itemsOwned<max_num_assets_per_contract
+    condition_5:bool = chain in blockchains
+    if condition_1 and condition_2 and condition_3 and condition_4 and condition_5:
         sendOffer(item)
-
 
 
 def run():
 
-    thread1 = threading.Thread(target=getAccessToken)
+    collection_info = getCollectionsData()
+
+    thread1 = threading.Thread(target=getAllAccessTokens)
     thread2 = threading.Thread(target=getBETHbalance)
 
     thread1.start()
@@ -47,13 +57,11 @@ def run():
 
     thread1.join()
     thread2.join()
-    
-    user_tokens = getUserAssets()
-    collection_info = getCollectionsData()
+
+    user_tokens = getUserAssetsUtils()
 
     orderUserData(user_tokens, collection_info)
     orderDataTakeBid(user_tokens)
-
 
     new_user_tokens = newDataTakeBid(user_tokens)
 
